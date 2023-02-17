9ddc1b58c7b5563e5ee5d140005f8f70c4ceaed8
Tanuj Datta <tanujdatta55@gmail.com>
2023-02-17 20:41:52 +0530

    first commit

---
+++ app/src/main/java/com/example/mallez/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mallez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+}
